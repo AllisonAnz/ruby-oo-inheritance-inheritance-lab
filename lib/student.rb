ec7bdccac7c1eb95582bef8c9c36a7e67bb19675
AllisonAnz <AllisonAnzalone@gmail.com>
2020-12-07 11:20:51 -0600

    commit tree 84952298ef1c876df2e32626cbe6b32e2a89510c

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+class Student < User
+    
+    def initialize
+        @knowledge = []
+    end
+
+    #takes in an argument of a string of knowledge and adds it to the studends knowledge array
+    def learn(knowledge)
+        @knowledge << knowledge
+    end
+
+    #returns that student's knowledge array
+    def knowledge
+        @knowledge
+    end
+
 
 end
--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,6 +1,13 @@
-class Teacher
+require 'pry'
+class Teacher < User
 
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
 
+  #returns a randome string of knowlege
+  #This method chooses a random object, lets say r
+  #from the elements of Array instances
+  def teach
+    KNOWLEDGE.sample
+  end
   
 end
